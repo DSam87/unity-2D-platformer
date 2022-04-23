2c9f189f49e6699341afa0726bda19ca6c93ab37
DSam87 <76795390+DSam87@users.noreply.github.com>
2022-04-23 17:39:15 -0500

    Added Double Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
 
     private bool isGrounded;
+    private bool anotherJump;
     public Transform groundCheckPoint;
     public LayerMask whatIsGround;
 
@@ -16,21 +17,30 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         // theRb = gameObject.GetComponent<Rigidbody2D>();
-
+        anotherJump = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         theRb.velocity = new Vector2(moveSpeed * Input.GetAxisRaw("Horizontal"), theRb.velocity.y);
-
         isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
 
         if(Input.GetButtonDown("Jump"))
         {
+
             if(isGrounded)
             {
-            theRb.velocity = new Vector2(theRb.velocity.x, jumpForce);
+                theRb.velocity = new Vector2(theRb.velocity.x, jumpForce);
+                anotherJump = true;
+            }
+            else
+            {
+                if(anotherJump == true)
+                {
+                    anotherJump = false;
+                    theRb.velocity = new Vector2(theRb.velocity.x, jumpForce);                
+                }
             }
         }
     }
